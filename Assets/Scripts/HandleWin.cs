5e080835e4341aeeeced24971028d8f421d1061d
Charith <crathnaya@wiley.com>
2021-11-23 21:20:09 +0530

    Added Particle effects for Finish Line

--- Assets/Scripts/HandleWin.cs
+++ Assets/Scripts/HandleWin.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class HandleWin : MonoBehaviour
 {
+    [SerializeField] ParticleSystem finishEffect = null;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
+            if(finishEffect != null){
+                finishEffect.Play();
+            }
             Invoke("ReloadScene", 1f);
         }
     }
